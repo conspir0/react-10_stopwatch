f8b08443a6fcbfbc4c10bc3f9aff1e337a96d8bb
conspir0 <michalczyk.mat3usz@gmail.com>
2020-09-09 23:20:40 +0200

    Create project structure.

---
+++ App.js
@@ -0,0 +1,16 @@
+class App extends React.Component {
+  state = {}
+
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          Hello World!
+        </div>
+      </React.Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
